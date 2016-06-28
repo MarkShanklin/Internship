d43e2562806d33f248c9ff29098cb8affc1751b3
Mark Anthony Shanklin <masisaries@gmail.com>
2016-06-28 14:51:03 -0700

    Fixed error in get_num.h

--- get_num.h
+++ get_num.h
@@ -10,7 +10,7 @@
 #define GET_NUM_H
 
 #define GN_NONNEG       01      /* Value must be >= 0 */
-#define GN_GT_O         02      /* Value must be > 0 */
+#define GN_GT_0         02      /* Value must be > 0 */
 
                                 /* By default, intergers are decimal */
 #define GN_ANY_BASE     0100    /* Can use any base - line strtol(3) */
