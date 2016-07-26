8924ed9c7ebba8a7e4bec2f80c19446eb77c7e45
mark.shanklin <mark.shanklin@oit.edu>
2016-07-26 11:49:51 -0700

    abstracted dir from submission system

--- src/cp_files.c
+++ src/cp_files.c
@@ -8,34 +8,46 @@
 #include <stdlib.h>
 #include <time.h>
 #include <libgen.h>
+#include <pwd.h>
 
-// You want to replace this with some code that calls
-// struct passwd *getpwuid(uid_t uid);
-#define FILE_DROP_DIR "/home/grader_dev/file_drop"
-#define LOG_FILE "/home/grader_dev/file_drop/log_file.txt"
+#define LOG_FILE "/log_file.txt"
+#define FILE_DROP_DIR "/file_drop"
+
 int main(int argc, char *argv[])
 {
   int i;
   int rfd;
   int wfd;
   char wfile_name[PATH_MAX];
+  char wfile_drop_dir[PATH_MAX];
+  char wlog_file[PATH_MAX]; 
   char buf[1000];
   ssize_t num_read;
   ssize_t num_written;
   FILE *lfp;
   time_t rawtime;
-  struct tm * timeinfo;
-
+  struct tm *timeinfo;
+  struct passwd *pwuid;
   time ( &rawtime );
   timeinfo = localtime ( &rawtime );
 
+  pwuid = getpwuid(geteuid());
+  memset(wfile_drop_dir, 0, PATH_MAX);
+  strncpy(wfile_drop_dir, pwuid->pw_dir, PATH_MAX);
+  strncat(wfile_drop_dir, FILE_DROP_DIR, 100);
+  fprintf(stdout, "%s\n", wfile_drop_dir);
+ 
+  memset(wlog_file, 0, PATH_MAX);
+  strncpy(wlog_file, pwuid->pw_dir, PATH_MAX); 
+  strncat(wlog_file, LOG_FILE, 100);
+  fprintf(stdout, "%s\n", wlog_file);
   printf("args %d\n", argc);
-  lfp = fopen(LOG_FILE, "a" );
+  lfp = fopen(wlog_file, "a" );
 
   for (i = 1; i < argc; i++) {
     if ((rfd = open(argv[i], O_RDONLY)) > 0) {
       memset(wfile_name, 0, PATH_MAX);
-      strncpy(wfile_name, FILE_DROP_DIR, PATH_MAX);
+      strncpy(wfile_name, wfile_drop_dir, PATH_MAX);
       strncat(wfile_name, "/", 100);
       strncat(wfile_name, argv[i], 100);
       printf("file %d <%s>\n", i, wfile_name);
