78273f0851d3dc8b632c8763a1863e197f1b37fd
Trall541 <jacob.shanklin@oit.edu>
2016-08-23 11:38:43 -0700

    fileObs minor fixes / tweeks

---
+++ TestController/TestController.c
@@ -0,0 +1,15 @@
+/******************************************
+* File Name: TestController.c
+* Purpose: Recieve a message via UDP, move
+* submitted files to a directory, execute tests on files.
+* Creation Date: 08-01-2016
+* Last Modified: Mon 01 Aug 2016 10:59:35 AM PDT
+* Created By: Jacob Shanklin
+*******************************************/
+
+
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
